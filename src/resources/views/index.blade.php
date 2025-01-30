b5164cdb086f2b65e6f690034eeb49b35a559e95
ren <tyatyareat14@gmail.com>
2025-01-30 22:43:19 +0900

    クリアされたキャッシュをコミット

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -13,15 +13,22 @@
             <input type="text" name="title" value="" />
         </div>
         <button class="button">
-            <input type="submit" value="送信" />
+            <input type="submit" value="作成" />
         </button>
     </div>
     <div class="form__group-title">
+        Todo
         <div class="form__group-list">
-            <input type="text" name="test" value="" />
+            <input type="text" name="test" value="test" />
         </div>
+        <button class="button__update">
+            <input type="submit" value="更新" />
+        </button>
+        <button class="button__delete">
+            <input type="submit" value="削除" />
+        </button>
         <div class="form__group-list">
-            <input type="text" name="test2" value="" />
+            <input type="text" name="test2" value="test2" />
         </div>
         <button class="button__update">
             <input type="submit" value="更新" />
